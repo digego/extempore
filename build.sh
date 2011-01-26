bec396930b94ba0d779b8141d45aa163fe3f701f
Andrew Sorensen <digegoo@gmail.com>
2011-01-26 17:52:27 +1000

    small changes to build

--- build.sh
+++ build.sh
@@ -6,17 +6,20 @@ echo "You must pass either 'linux' or 'osx'"
 return
 fi
 
+LLVM_DIR=""
+
 if [ $# -lt 2 ]
 then
-echo "You must provide a path to llvm"
-return
+echo "You can optionally provide a path to llvm as second argument"
+else
+LLVM_DIR=$2
 fi
 
 if [ $1 = "osx" ]
 then
 echo "compiling for osx ..."
 # COMPILE OSX
-g++ -w -O3 -D_GNU_SOURCE -DTARGET_OS_MAC  -D__STDC_CONSTANT_MACROS -D__STDC_LIMIT_MACROS -Iinclude -I$2/include src/*.cpp -L$2/Release/lib -lpthread -lm -lLLVMX86AsmParser -lLLVMX86AsmPrinter -lLLVMX86CodeGen -lLLVMSelectionDAG -lLLVMAsmParser -lLLVMAsmPrinter -lLLVMXCoreInfo -lLLVMX86Info -lLLVMInterpreter -lLLVMJIT -lLLVMExecutionEngine -lLLVMCodeGen -lLLVMScalarOpts -lLLVMInstCombine -lLLVMipo -lLLVMTransformUtils -lLLVMInstrumentation -lLLVMipa -lLLVMAnalysis -lLLVMTarget -lLLVMMC -lLLVMCore -lLLVMSupport -lLLVMSystem -lpcre -L/Users/andrew/Documents/Code/glfw-2.7/lib/cocoa -lglfw -framework opengl -framework glut -framework cocoa -framework coreaudio -o extempore
+g++ -w -O3 -D_GNU_SOURCE -DTARGET_OS_MAC  -D__STDC_CONSTANT_MACROS -D__STDC_LIMIT_MACROS -Iinclude -I$LLVM_DIR/include src/*.cpp -L$LLVM_DIR/Release/lib -lpthread -lm -lLLVMX86AsmParser -lLLVMX86AsmPrinter -lLLVMX86CodeGen -lLLVMSelectionDAG -lLLVMAsmParser -lLLVMAsmPrinter -lLLVMXCoreInfo -lLLVMX86Info -lLLVMInterpreter -lLLVMJIT -lLLVMExecutionEngine -lLLVMCodeGen -lLLVMScalarOpts -lLLVMInstCombine -lLLVMipo -lLLVMTransformUtils -lLLVMInstrumentation -lLLVMipa -lLLVMAnalysis -lLLVMTarget -lLLVMMC -lLLVMCore -lLLVMSupport -lLLVMSystem -lpcre -lglfw -framework opengl -framework glut -framework cocoa -framework coreaudio -o extempore
 echo "done"
 else
 echo "compiling for linux ..."
