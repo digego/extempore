552a9fe322a38d0d74ead3885ebbf89afde8895e
Andrew Sorensen <andrew@ubuntu.(none)>
2011-01-27 17:10:45 +1000

    Slightly spunked up the example ... only slightly ;)

--- examples/opengl_101.scm
+++ examples/opengl_101.scm
@@ -16,15 +16,18 @@
   (lambda (degree)
     (glClear (+ GL_COLOR_BUFFER_BIT GL_DEPTH_BUFFER_BIT))
     (glLoadIdentity)
-    (glRotated degree 1.0 1.0 0.0)
-    (glutWireCube 0.25)
+    (glTranslated 0.0 -1.0 0.0)
+    (dotimes (i 50)
+       (glTranslated (/ (/ (i64tod i) 50.0) 10.0) 0.0 0.0) 
+       (glRotated degree (/ (i64tod i) 50.0) 0.5 0.0)
+       (glutWireCube 0.1))
     (glfwSwapBuffers)))
 
 ;; standard impromptu callback                                                
 (define opengl-test
   (lambda (degree)
     (my-gl-loop degree)
-    (callback (+ (now) 1000) 'opengl-test (+ degree 1.0))))
+    (callback (+ (now) 500) 'opengl-test (+ degree 1.0))))
 
 ;; open window
 (open-window 640 480)
