b170a31923764a6c6dec7492f9235718a0ccd023
Andrew Sorensen <digegoo@gmail.com>
2011-03-29 10:22:21 +1000

    Updated osc example with multiple ports

--- examples/osc_101.scm
+++ examples/osc_101.scm
@@ -3,16 +3,22 @@
 ; Simplest possible OSC example
 ;
 
-;; start osc server on port 7009
-(io:osc:start-server 7009)
+;; start two osc servers
+(io:osc:start-server 7009 "osc-receive-7009")
+(io:osc:start-server 7019 "osc-receive-7019")
 
 ; Prints all messages to the log
-(define (io:osc:receive timestamp address . args)
-   (print address '-> args))
+(define (osc-receive-7009 timestamp address . args)
+   (println 'port 7009 address '-> args))
+
+(define (osc-receive-7019 timestamp address . args)
+   (println 'port 7019 address '-> args))
 
 
 ;; define a sending address
-(define addy (cons "localhost" 7009))
+(define addy1 (cons "localhost" 7009))
+(define addy2 (cons "localhost" 7019))
 
 ;; Some a test message delayed by 4 seconds
 (io:osc:send (+ (now) (* *second* 4)) addy "/test/msg" "Hello" 500 6.6 "World")
+(io:osc:send (+ (now) (* *second* 4)) addy2 "/test/msg" "Hello" 500 6.6 "World")
