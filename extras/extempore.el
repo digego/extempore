3b0f19fb8675b128e064d5e20659937b295a2abd
Ben Swift <benjamin.j.swift@gmail.com>
2012-06-22 13:56:10 +1000

    extempore.el is now a major mode (doesn't quite work yet, though)

--- extras/extempore.el
+++ extras/extempore.el
@@ -1,118 +1,364 @@
-;; This code kindly supplied originally by Hector Levesque
+;;; extempore.el
+
+;; Author: Ben Swift <ben.swift@anu.edu.au>
+;; Adapted from: scheme.el
+;; Also includes some work done by Hector Levesque and Andrew Sorensen
+
+;; TODO add licence info
+
+;; Commentary:
+
+;; The major mode for editing Extempore code, which is a mixture of
+;; Scheme and xtlang - a strongly-typed, scheme-like language.  The
+;; Extempore project page is at http://github.com/digego/extempore
 ;;
-;; Put (autoload 'extempore-mode "/path/to/thisfile" "" t) in .emacs.
-;;
-;; This mode uses the normal Scheme key bindings except that the sequence
-;;    control-x control-x sends a top level def to Extempore
-;;    control-x control-r sends a region of defs to Extempore
-;;
-;; Future work: get TAB completion to work!
+;; TODO In the future this mode will also provide functionality for
+;; running and interacting with an inferior Extempore process in
+;; another Emacs buffer.
 
-(require 'scheme)
+;;; Code:
+
+(require 'lisp-mode)
 
-(defvar extempore-keydef "\C-x\C-x")     ; key sequence to send a definition
-(defvar extempore-keyreg "\C-x\C-r")     ; key sequence to send a region
-(defvar extempore-port 7099)             ; TCP port to Extempore
-;(defvar extempore-process nil)           ; process during TCP connection
-;(make-local-variable 'extempore-process)
-(make-variable-buffer-local 'extempore-process)
-(setq extempore-process nil)
+(defvar extempore-mode-syntax-table
+  (let ((st (make-syntax-table))
+	(i 0))
+    ;; Symbol constituents
+    ;; We used to treat chars 128-256 as symbol-constituent, but they
+    ;; should be valid word constituents (Bug#8843).  Note that valid
+    ;; identifier characters are Scheme-implementation dependent.
+    (while (< i ?0)
+      (modify-syntax-entry i "_   " st)
+      (setq i (1+ i)))
+    (setq i (1+ ?9))
+    (while (< i ?A)
+      (modify-syntax-entry i "_   " st)
+      (setq i (1+ i)))
+    (setq i (1+ ?Z))
+    (while (< i ?a)
+      (modify-syntax-entry i "_   " st)
+      (setq i (1+ i)))
+    (setq i (1+ ?z))
+    (while (< i 128)
+      (modify-syntax-entry i "_   " st)
+      (setq i (1+ i)))
 
-(defun smart-tab ()
-  "This smart tab is minibuffer compliant: it acts as usual in
-    the minibuffer. Else, if mark is active, indents region. Else if
-    point is at the end of a symbol, expands it. Else indents the
-    current line."
-  (interactive)
-  (if (minibufferp)
-      (unless (minibuffer-complete)
-	(dabbrev-expand nil))
-    (if mark-active
-	(indent-region (region-beginning)
-		       (region-end))
-	(if (looking-at "\\_>")
-	    (dabbrev-expand nil)
-	  (indent-for-tab-command)))))
+    ;; Whitespace
+    (modify-syntax-entry ?\t "    " st)
+    (modify-syntax-entry ?\n ">   " st)
+    (modify-syntax-entry ?\f "    " st)
+    (modify-syntax-entry ?\r "    " st)
+    (modify-syntax-entry ?\s "    " st)
 
-;(global-set-key (kbd "TAB") 'smart-tab)
-(define-key scheme-mode-map (kbd "TAB") 'smart-tab); # only in scheme-mode
+    ;; These characters are delimiters but otherwise undefined.
+    ;; Brackets and braces balance for editing convenience.
+    (modify-syntax-entry ?\[ "(]  " st)
+    (modify-syntax-entry ?\] ")[  " st)
+    (modify-syntax-entry ?{ "(}  " st)
+    (modify-syntax-entry ?} "){  " st)
+    (modify-syntax-entry ?\| "\" 23bn" st)
+    ;; Guile allows #! ... !# comments.
+    ;; But SRFI-22 defines the comment as #!...\n instead.
+    ;; Also Guile says that the !# should be on a line of its own.
+    ;; It's too difficult to get it right, for too little benefit.
+    ;; (modify-syntax-entry ?! "_ 2" st)
 
-(define-key scheme-mode-map (kbd "RET") 'newline-and-indent)
+    ;; Other atom delimiters
+    (modify-syntax-entry ?\( "()  " st)
+    (modify-syntax-entry ?\) ")(  " st)
+    ;; It's used for single-line comments as well as for #;(...) sexp-comments.
+    (modify-syntax-entry ?\; "< 2 " st)
+    (modify-syntax-entry ?\" "\"   " st)
+    (modify-syntax-entry ?' "'   " st)
+    (modify-syntax-entry ?` "'   " st)
 
-(defun ext:m1 (name duration)
-  (interactive "sName: \nsDuration: ")
-  (insert (concat "(define " name
-		  "\n  (lambda (beat dur)\n    "
-		  "(callback (*metro* (+ beat (* .5 " duration "))) '" 
-		  name " (+ beat " duration ") " duration ")))\n\n"
-		  "(" name " (*metro* 'get-beat 4) " duration ")")))
+    ;; Special characters
+    (modify-syntax-entry ?, "'   " st)
+    (modify-syntax-entry ?@ "'   " st)
+    (modify-syntax-entry ?# "' 14" st)
+    (modify-syntax-entry ?\\ "\\   " st)
+    st))
+
+(defvar extempore-mode-abbrev-table nil)
+(define-abbrev-table 'extempore-mode-abbrev-table ())
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; add some stuff to scheme mode
+;; imenu stuff goes here in scheme-mode - not implemented for
+;; extempore mode yet.
 
-(font-lock-add-keywords 'scheme-mode
-  '(("definec" . font-lock-keyword-face)
-    ("bind-func" . font-lock-keyword-face)
-    ("definec:dsp" . font-lock-keyword-face)
-    ("define-instrument" . font-lock-keyword-face)
-    ("bind-val" . font-lock-keyword-face)
-    ("bind-typevar" . font-lock-keyword-face)
-    ("bind-alias" . font-lock-keyword-face)
-    ("bind-type" . font-lock-keyword-face)    
-    ("bind-poly" . font-lock-keyword-face)
-    ("dotimes" . font-lock-keyword-face)
-    ("memzone" . font-lock-keyword-face)
-    ("bind-lib" . font-lock-keyword-face)))
+(defun extempore-mode-variables ()
+  (set-syntax-table extempore-mode-syntax-table)
+  (setq local-abbrev-table extempore-mode-abbrev-table)
+  (set (make-local-variable 'paragraph-start) (concat "$\\|" page-delimiter))
+  (set (make-local-variable 'paragraph-separate) paragraph-start)
+  (set (make-local-variable 'paragraph-ignore-fill-prefix) t)
+  (set (make-local-variable 'fill-paragraph-function) 'lisp-fill-paragraph)
+  ;; Adaptive fill mode gets in the way of auto-fill,
+  ;; and should make no difference for explicit fill
+  ;; because lisp-fill-paragraph should do the job.
+  (set (make-local-variable 'adaptive-fill-mode) nil)
+  (set (make-local-variable 'indent-line-function) 'lisp-indent-line)
+  (set (make-local-variable 'parse-sexp-ignore-comments) t)
+  (set (make-local-variable 'outline-regexp) ";;; \\|(....")
+  (set (make-local-variable 'comment-start) ";")
+  (set (make-local-variable 'comment-add) 1)
+  ;; Look within the line for a ; following an even number of backslashes
+  ;; after either a non-backslash or the line beginning.
+  (set (make-local-variable 'comment-start-skip)
+       "\\(\\(^\\|[^\\\\\n]\\)\\(\\\\\\\\\\)*\\);+[ \t]*")
+  (set (make-local-variable 'font-lock-comment-start-skip) ";+ *")
+  (set (make-local-variable 'comment-column) 40)
+  (set (make-local-variable 'parse-sexp-ignore-comments) t)
+  (set (make-local-variable 'lisp-indent-function) 'extempore-indent-function)
+  (setq mode-line-process '("" extempore-mode-line-process))
+  (set (make-local-variable 'imenu-case-fold-search) t)
+  (set (make-local-variable 'font-lock-defaults)
+       '((extempore-font-lock-keywords
+	  extempore-font-lock-keywords-1 extempore-font-lock-keywords-2)
+	 nil t (("+-*/.<>=!?$%_&~^:" . "w") (?#. "w 14"))
+	 beginning-of-defun
+	 (font-lock-mark-block-function . mark-defun)
+	 (font-lock-syntactic-face-function
+	  . extempore-font-lock-syntactic-face-function)
+	 (parse-sexp-lookup-properties . t)
+	 (font-lock-extra-managed-props syntax-table)))
+  (set (make-local-variable 'lisp-doc-string-elt-property)
+       'extempore-doc-string-elt))
 
-(put 'dotimes 'scheme-indent-function 1)
-(put 'bind-func 'scheme-indent-function 1)
+(defvar extempore-mode-line-process "")
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; handle extempore minor mode stuff
+(defvar extempore-mode-map
+  (let ((smap (make-sparse-keymap))
+	(map (make-sparse-keymap "Extempore")))
+    (set-keymap-parent smap lisp-mode-shared-map)
+    (define-key smap [menu-bar extempore] (cons "Extempore" map))
+    (define-key map [run-extempore] '("Run Inferior Extempore" . run-extempore))
+    (define-key map [uncomment-region]
+      '("Uncomment Out Region" . (lambda (beg end)
+				   (interactive "r")
+				   (comment-region beg end '(4)))))
+    (define-key map [comment-region] '("Comment Out Region" . comment-region))
+    (define-key map [indent-region] '("Indent Region" . indent-region))
+    (define-key map [indent-line] '("Indent Line" . lisp-indent-line))
+    (put 'comment-region 'menu-enable 'mark-active)
+    (put 'uncomment-region 'menu-enable 'mark-active)
+    (put 'indent-region 'menu-enable 'mark-active)
+    smap)
+  "Keymap for Extempore mode.
+All commands in `lisp-mode-shared-map' are inherited by this map.")
+
+;;;###autoload
+(define-derived-mode extempore-mode prog-mode "Extempore"
+  "Major mode for editing Extempore code.
+This mode has been adapted from `scheme-mode'.
 
-(define-minor-mode extempore-mode 
-   "Toggle the mode for interacting with Scheme in Extempore over TCP"
-   :init-value nil :lighter " Extempore" :keymap scheme-mode-map
-   (if extempore-mode (extempore-connect "localhost" 7099) (extempore-stop)))
+In addition, if an inferior Extempore process is running, some additional
+commands will be defined, for evaluating expressions and controlling
+the interpreter, and the state of the process will be displayed in the
+mode line of all Extempore buffers. Use \\[run-extempore] to
+start an inferior Extempore.
 
-(defun extempore-connect (host port)     ; start connection to Extempore
-  (interactive "sHostname: \nnPort: ")
-  (define-key scheme-mode-map extempore-keydef 'extempore-send-definition)
-  (define-key scheme-mode-map extempore-keyreg 'extempore-send-region)
-  (if (not (null extempore-process))
-      (delete-process extempore-process))
-  (setq extempore-process
-	(open-network-stream "extempore" nil
-			     (if (null host) "localhost" host)
-			     (if (null port) 7099 port)))
-  (set-process-filter extempore-process 
-	'(lambda (proc str) (message (substring str 0 -1)))))
+Entry to this mode calls the value of `extempore-mode-hook'.")
 
-(defun extempore-stop ()                 ; terminate connection to Extempore
-  (interactive)
-  (delete-process extempore-process)
-  (setq extempore-process nil))
+(defgroup extempore nil
+  "Editing Extempore code."
+  :link '(custom-group-link :tag "Font Lock Faces group" font-lock-faces)
+  :group 'lisp)
 
-(defun extempore-send-definition ()
-  "Send the enclosing top-level def to Extempore server for evaluation"
-  (interactive)
-  (save-excursion
-    (mark-defun)
-    (let ((str (concat (buffer-substring (point) (mark))
-                       "\r\n")))
-      (process-send-string extempore-process str)       
-      ; (process-send-region extempore-process (point) (mark))
-      (redisplay)                          ; flash the def like Extempore
-      (sleep-for .25))))
+(defcustom extempore-mode-hook nil
+  "Normal hook run when entering `extempore-mode'.
+See `run-hooks'."
+  :type 'hook
+  :group 'extempore)
 
-(defun extempore-send-region ()
-  "Send the current region (or all the buffer) to Extempore for evaluation"
-  (interactive)
-  (save-excursion 
-    (if mark-active
-	(unless (= (point) (region-beginning)) (exchange-point-and-mark))
-      (progn (goto-char (point-min)) (set-mark (point-max))))
-    (let ((start (region-beginning)) (end (region-end)))
-      (while (re-search-forward "^[^\n;]*(" end t)
-	(extempore-send-definition)
-	(end-of-defun)))))
+(defconst extempore-font-lock-keywords-1
+  (eval-when-compile
+    (list
+     ;; Declarations
+     (list (concat
+	    ;; scheme
+	    "(\\(define\\(c\\|-syntax\\|-macro\\|-instrument\\|-sampler\\)\\*?\\|"
+	    ;; xtlang
+	    "(bind-\\(func\\|val\\|type\\|alias\\|poly\\|lib\\)\\|"
+	    ;; memzone
+	    "memzone\\)\\>"
+	    ;; Any whitespace and declared object.
+	    "[ \t]*(?"
+	    "\\(\\sw+\\)?")
+	   '(1 font-lock-keyword-face)
+	   '(6 (cond ((match-beginning 3) font-lock-function-name-face)
+		     ((match-beginning 5) font-lock-variable-name-face)
+		     (t font-lock-type-face))
+	       nil t))
+     ))
+  "Subdued expressions to highlight in Extempore modes.")
+
+(defconst extempore-font-lock-keywords-2
+  (append extempore-font-lock-keywords-1
+   (eval-when-compile
+     (list
+      ;; Control structures.
+      (cons
+       (concat
+	"(" (regexp-opt
+	     '("begin" "call-with-current-continuation" "call/cc"
+	       "call-with-input-file" "call-with-output-file" "case" "cond"
+	       "do" "dotimes" "else" "for-each" "if" "lambda"
+	       "let" "let*" "let-syntax" "letrec" "letrec-syntax"
+	       "and" "or"
+	       "map" "syntax" "syntax-rules") t)
+	"\\>") 1)
+      ;; It wouldn't be Scheme w/o named-let.
+      '("(let\\s-+\\(\\sw+\\)"
+	(1 font-lock-function-name-face))
+      )))
+  "Gaudy expressions to highlight in Extempore modes.")
+
+(defvar extempore-font-lock-keywords extempore-font-lock-keywords-1
+  "Default expressions to highlight in Extempore modes.")
+
+(defconst extempore-sexp-comment-syntax-table
+  (let ((st (make-syntax-table extempore-mode-syntax-table)))
+    (modify-syntax-entry ?\; "." st)
+    (modify-syntax-entry ?\n " " st)
+    (modify-syntax-entry ?#  "'" st)
+    st))
+
+(put 'lambda 'scheme-doc-string-elt 2)
+;; Docstring's pos in a `define' depends on whether it's a var or fun def.
+(put 'define 'scheme-doc-string-elt
+     (lambda ()
+       ;; The function is called with point right after "define".
+       (forward-comment (point-max))
+       (if (eq (char-after) ?\() 2 0)))
+
+(defun extempore-font-lock-syntactic-face-function (state)
+  (when (and (null (nth 3 state))
+	     (eq (char-after (nth 8 state)) ?#)
+	     (eq (char-after (1+ (nth 8 state))) ?\;))
+    ;; It's a sexp-comment.  Tell parse-partial-sexp where it ends.
+    (save-excursion
+      (let ((pos (point))
+	    (end
+	     (condition-case err
+		 (let ((parse-sexp-lookup-properties nil))
+		   (goto-char (+ 2 (nth 8 state)))
+		   ;; FIXME: this doesn't handle the case where the sexp
+		   ;; itself contains a #; comment.
+		   (forward-sexp 1)
+		   (point))
+	       (scan-error (nth 2 err)))))
+	(when (< pos (- end 2))
+	  (put-text-property pos (- end 2)
+			     'syntax-table extempore-sexp-comment-syntax-table))
+	(put-text-property (- end 1) end 'syntax-table '(12)))))
+  ;; Choose the face to use.
+  (lisp-font-lock-syntactic-face-function state))
+
+(defvar calculate-lisp-indent-last-sexp)
+
+;; FIXME this duplicates almost all of lisp-indent-function.
+;; Extract common code to a subroutine.
+(defun extempore-indent-function (indent-point state)
+  "Extempore mode function for the value of the variable `lisp-indent-function'.
+This behaves like the function `lisp-indent-function', except that:
+
+i) it checks for a non-nil value of the property `extempore-indent-function'
+\(or the deprecated `extempore-indent-hook'), rather than `lisp-indent-function'.
+
+ii) if that property specifies a function, it is called with three
+arguments (not two), the third argument being the default (i.e., current)
+indentation."
+  (let ((normal-indent (current-column)))
+    (goto-char (1+ (elt state 1)))
+    (parse-partial-sexp (point) calculate-lisp-indent-last-sexp 0 t)
+    (if (and (elt state 2)
+	     (not (looking-at "\\sw\\|\\s_")))
+	;; car of form doesn't seem to be a symbol
+	(progn
+	  (if (not (> (save-excursion (forward-line 1) (point))
+		      calculate-lisp-indent-last-sexp))
+	      (progn (goto-char calculate-lisp-indent-last-sexp)
+		     (beginning-of-line)
+		     (parse-partial-sexp (point)
+					 calculate-lisp-indent-last-sexp 0 t)))
+	  ;; Indent under the list or under the first sexp on the same
+	  ;; line as calculate-lisp-indent-last-sexp.  Note that first
+	  ;; thing on that line has to be complete sexp since we are
+	  ;; inside the innermost containing sexp.
+	  (backward-prefix-chars)
+	  (current-column))
+      (let ((function (buffer-substring (point)
+					(progn (forward-sexp 1) (point))))
+	    method)
+	(setq method (or (get (intern-soft function) 'extempore-indent-function)
+			 (get (intern-soft function) 'extempore-indent-hook)))
+	(cond ((or (eq method 'defun)
+		   (and (null method)
+			(> (length function) 3)
+			(string-match "\\`def" function)))
+	       (lisp-indent-defform state indent-point))
+	      ((integerp method)
+	       (lisp-indent-specform method state
+				     indent-point normal-indent))
+	      (method
+		(funcall method state indent-point normal-indent)))))))
+
+
+;;; Let is different in Scheme
+
+(defun would-be-symbol (string)
+  (not (string-equal (substring string 0 1) "(")))
+
+(defun next-sexp-as-string ()
+  ;; Assumes that it is protected by a save-excursion
+  (forward-sexp 1)
+  (let ((the-end (point)))
+    (backward-sexp 1)
+    (buffer-substring (point) the-end)))
+
+;; This is correct but too slow.
+;; The one below works almost always.
+;;(defun extempore-let-indent (state indent-point)
+;;  (if (would-be-symbol (next-sexp-as-string))
+;;      (extempore-indent-specform 2 state indent-point)
+;;      (extempore-indent-specform 1 state indent-point)))
+
+(defun extempore-let-indent (state indent-point normal-indent)
+  (skip-chars-forward " \t")
+  (if (looking-at "[-a-zA-Z0-9+*/?!@$%^&_:~]")
+      (lisp-indent-specform 2 state indent-point normal-indent)
+    (lisp-indent-specform 1 state indent-point normal-indent)))
+
+;; (put 'begin 'extempore-indent-function 0), say, causes begin to be indented
+;; like defun if the first form is placed on the next line, otherwise
+;; it is indented like any other form (i.e. forms line up under first).
+
+(put 'begin 'extempore-indent-function 0)
+(put 'case 'extempore-indent-function 1)
+(put 'delay 'extempore-indent-function 0)
+(put 'do 'extempore-indent-function 2)
+(put 'lambda 'extempore-indent-function 1)
+(put 'let 'extempore-indent-function 'extempore-let-indent)
+(put 'let* 'extempore-indent-function 1)
+(put 'letrec 'extempore-indent-function 1)
+(put 'let-values 'extempore-indent-function 1) ; SRFI 11
+(put 'let*-values 'extempore-indent-function 1) ; SRFI 11
+(put 'sequence 'extempore-indent-function 0) ; SICP, not r4rs
+(put 'let-syntax 'extempore-indent-function 1)
+(put 'letrec-syntax 'extempore-indent-function 1)
+(put 'syntax-rules 'extempore-indent-function 1)
+(put 'syntax-case 'extempore-indent-function 2) ; not r5rs
+
+(put 'call-with-input-file 'extempore-indent-function 1)
+(put 'with-input-from-file 'extempore-indent-function 1)
+(put 'with-input-from-port 'extempore-indent-function 1)
+(put 'call-with-output-file 'extempore-indent-function 1)
+(put 'with-output-to-file 'extempore-indent-function 1)
+(put 'with-output-to-port 'extempore-indent-function 1)
+(put 'call-with-values 'extempore-indent-function 1) ; r5rs?
+(put 'dynamic-wind 'extempore-indent-function 3) ; r5rs?
+
+
+(provide 'extempore-major-mode)
+
+;;; extempore-major-mode.el ends here
