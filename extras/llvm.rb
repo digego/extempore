7242e4d0367ed48c1db9463362f1bc077c491f81
Ben Swift <benjamin.j.swift@gmail.com>
2013-03-20 14:24:38 +1100

    tidied up LLVM homebrew formula

--- extras/llvm.rb
+++ extras/llvm.rb
@@ -1,38 +1,12 @@
 require 'formula'
 
-class Clang < Formula
-  homepage  'http://llvm.org/'
-  url       'http://llvm.org/releases/3.2/clang-3.2.src.tar.gz'
-  sha1      'b0515298c4088aa294edc08806bd671f8819f870'
-
-  head      'http://llvm.org/git/clang.git'
-end
-
-class CompilerRt < Formula
-  homepage  'http://llvm.org/'
-  url       'http://llvm.org/releases/3.2/compiler-rt-3.2.src.tar.gz'
-  sha1      '718c0249a00e928f8bba32c84771da998ea4d42f'
-
-  head      'http://llvm.org/git/compiler-rt.git'
-end
-
 class Llvm < Formula
   homepage  'http://llvm.org/'
   url       'http://llvm.org/releases/3.2/llvm-3.2.src.tar.gz'
   sha1      '42d139ab4c9f0c539c60f5ac07486e9d30fc1280'
+  keg_only "Specially patched LLVM for use in building Extempore."
 
-  head      'http://llvm.org/git/llvm.git'
-
-  bottle do
-    sha1 '9fddf0bfed060ade86c88b64c40cc4bd5f1f0f2c' => :mountainlion
-    sha1 '8d3f3d5090d3bd2cf0c953da105efc63080e948f' => :lion
-    sha1 '3ba0beee27d60e80e9790cca9d5e21b2b8169ffe' => :snowleopard
-  end
-
-  option :universal
-  option 'with-clang', 'Build Clang C/ObjC/C++ frontend'
   option 'with-asan', 'Include support for -faddress-sanitizer (from compiler-rt)'
-  option 'shared', 'Build LLVM as a shared library'
   option 'all-targets', 'Build all target backends'
   option 'rtti', 'Build with C++ RTTI'
   option 'disable-assertions', 'Speeds up LLVM, but provides less debug information'
@@ -43,23 +17,9 @@ class Llvm < Formula
   end
 
   def install
-    if build.universal? and build.include? 'shared'
-      onoe "Cannot specify both shared and universal (will not build)"
-      exit 1
-    end
-
-    Clang.new("clang").brew do
-      clang_dir.install Dir['*']
-    end if build.include? 'with-clang'
-
     CompilerRt.new("compiler-rt").brew do
       (buildpath/'projects/compiler-rt').install Dir['*']
     end if build.include? 'with-asan'
-
-    if build.universal?
-      ENV['UNIVERSAL'] = '1'
-      ENV['UNIVERSAL_ARCH'] = 'i386 x86_64'
-    end
 
     ENV['REQUIRES_RTTI'] = '1' if build.include? 'rtti'
 
@@ -76,7 +36,6 @@ class Llvm < Formula
     else
       args << "--enable-targets=host"
     end
-    args << "--enable-shared" if build.include? 'shared'
 
     args << "--disable-assertions" if build.include? 'disable-assertions'
 
@@ -85,13 +44,6 @@ class Llvm < Formula
 
     # install llvm python bindings
     (share/'llvm/bindings').install buildpath/'bindings/python'
-
-    # install clang tools and bindings
-    cd clang_dir do
-      system 'make install'
-      (share/'clang/tools').install 'tools/scan-build', 'tools/scan-view'
-      (share/'clang/bindings').install 'bindings/python'
-    end if build.include? 'with-clang'
   end
 
   def test
@@ -99,17 +51,14 @@ class Llvm < Formula
   end
 
   def caveats; <<-EOS.undent
-    Extra tools and bindings are installed in #{share}/llvm and #{share}/clang.
+    This is a specifically patched version of LLVM for building Extempore.
+    It shouldn't get in the way of the Xcode-provided LLVM install.
 
+    If you have any problems, raise them on extemporelang@googlegroups.com
     If you already have LLVM installed, then "brew upgrade llvm" might not work.
-    Instead, try:
-        brew rm llvm && brew install llvm
     EOS
   end
 
-  def clang_dir
-    buildpath/'tools/clang'
-  end
 end
 
 __END__
