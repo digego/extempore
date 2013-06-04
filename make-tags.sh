4b87270629796968a82960a80d3e53d79a043e38
Ben Swift <benjamin.j.swift@gmail.com>
2013-06-04 17:11:09 +1000

    TAGS file: allow underscores in xtlang func names

--- make-tags.sh
+++ make-tags.sh
@@ -2,4 +2,4 @@
 
 rm TAGS
 find -E . -regex '.*/.*\.(cpp|h)$' -print | etags -
-find -E . -regex '.*/.*\.xtm$' -print | etags --append --regex='/(bind-[a-z]* \([a-z-!]+\)/\1/' --language=scheme -
+find -E . -regex '.*/.*\.xtm$' -print | etags --append --regex='/(bind-[a-z]* \([a-z-_!]+\)/\1/' --language=scheme -
