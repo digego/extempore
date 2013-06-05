7af7ab19429bf9f2b394cab5ed26123af4480789
Ben Swift <benjamin.j.swift@gmail.com>
2013-06-05 12:22:14 +1000

    add C-x C-l keybinding to toggle logger minor mode

--- extras/extempore.el
+++ extras/extempore.el
@@ -266,7 +266,8 @@ See `run-hooks'."
   (define-key keymap (kbd "C-M-x") 'extempore-send-defn-at-point)
   (define-key keymap (kbd "C-x C-r") 'extempore-send-region)
   (define-key keymap (kbd "C-x C-b") 'extempore-send-buffer)
-  (define-key keymap (kbd "C-x y") 'extempore-toggle-tr-animation))
+  (define-key keymap (kbd "C-x y") 'extempore-toggle-tr-animation)
+  (define-key keymap (kbd "C-x C-l") 'extempore-logger-mode))
 
 (extempore-keybindings extempore-mode-map)
 
