74843f377c7a45aa662ad9e968e11cef71b7713a
Ben Swift <benjamin.j.swift@gmail.com>
2013-07-05 11:42:51 +1000

    make-dist.sh script now packages up libraries in runtime/lib

--- extras/make-dist.sh
+++ extras/make-dist.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# package up the binary extempore executable
+# create a binary extempore archive for distribution
 
 case $(uname) in
     Linux) SHLIB_EXT=so ;;
@@ -7,18 +7,24 @@ case $(uname) in
     *) echo Cannot package for OS:  $(uname) >&2 ; exit 1 ;;
 esac
 
-DIST_DIR=extempore-$(uname)-$(date "+%Y.%m.%d")
-DIST_SHLIBS="assimp cairo rtmidi"
+DIST_ARCHIVE=extempore-$(uname)-$(date "+%Y.%m.%d")
+DIST_FILES="extempore libs examples extras README.md"
+DIST_SHLIBS="libassimp libcairo librtmidi"
 
-echo "Creating extempore binary archive in ${DIST_DIR}"
+echo "Creating extempore distribution archive ${DIST_ARCHIVE}"
 
-mkdir $DIST_DIR
+mkdir -p $DIST_ARCHIVE/runtime/lib
 
-# list of files to go in the binary distribution
-DIST_FILES="extempore runtime libs examples extras make-tags.sh README.md"
+cp -R $DIST_FILES $DIST_ARCHIVE
+# need to handle runtime specially (because of shlibs)
+cp runtime/* $DIST_ARCHIVE/runtime
+# copy the shlibs into runtime/lib
+for f in $DIST_SHLIBS
+do
+  cp -f runtime/lib/$f.$SHLIB_EXT $DIST_ARCHIVE/runtime/lib
+done
+# tar up the archive
+tar -cvzf ${DIST_ARCHIVE}.tar.gz $DIST_ARCHIVE
+rm -rf $DIST_ARCHIVE
 
-cp -R $DIST_FILES $DIST_DIR
-tar -cvzf ${DIST_DIR}.tar.gz $DIST_DIR
-rm -r $DIST_DIR
-
-echo "sha1 $(shasum ${DIST_DIR}.tar.gz) (needed for homebrew formula)"
+echo "sha1: $(shasum ${DIST_ARCHIVE}.tar.gz)"
