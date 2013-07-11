b252694c0bfa0b73ee72309c858f45e61ba3a59d
Ben Swift <benjamin.j.swift@gmail.com>
2013-07-12 09:40:19 +1000

    improved make-tags.sh

--- extras/make-tags.sh
+++ extras/make-tags.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
-rm TAGS
+[ -f TAGS ] && rm TAGS
+
+# make extempore/xtlang tags
 find -E . -regex '.*/.*\.(cpp|h)$' -print | etags -
 find -E . -regex '.*/.*\.xtm$' -print | etags --append --regex='/(bind-[a-z]* \([a-z-_!]+\)/\1/' --language=scheme -
+find -E . -regex '.*/.*\.xtm$' -print | etags --append --regex='/(bind-lib [a-z-_!]+ \([a-z-_!]+\)/\1/' -
