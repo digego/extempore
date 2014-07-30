5bf5d06e056bd62c15b0fdde55498489ddc0c761
Andrew Sorensen <digegoo@gmail.com>
2014-07-31 09:47:42 +1000

    stdlib compilation updates

--- compile-stdlib.sh
+++ compile-stdlib.sh
@@ -34,17 +34,17 @@ PRECOMP_EXTEMPORE_RUN_COMMAND="./extempore --run "
 # check all the required shared libs are there
 for f in $PRECOMP_LIBS
 do
-    echo "(sys:precomp:compile-xtm-file \"libs/$f\" #t #t)" > $PRECOMP_COMMAND_FILENAME
+    echo "(sys:precomp:compile-xtm-file \"libs/$f\" #t #t #t)" > $PRECOMP_COMMAND_FILENAME
     $PRECOMP_EXTEMPORE_RUN_COMMAND $PRECOMP_COMMAND_FILENAME
-    rc=$?
-    if [[ $rc != 0 ]] ; then
-        echo
-        echo Error precompiling $f
-        exit $rc
-    else
-        echo
-        echo Successfully precompiled $f
-    fi
+    # rc=$?
+    # if [[ $rc != 0 ]] ; then
+    #     echo
+    #     echo Error precompiling $f
+    #     exit $rc
+    # else
+    #     echo
+    #     echo Successfully precompiled $f
+    # fi
 done
 
 rm $PRECOMP_COMMAND_FILENAME
