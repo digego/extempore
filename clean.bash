7fcc26eb920ea0fc4c10a89c9968f774e2a69ffd
Ben Swift <benjamin.j.swift@gmail.com>
2014-08-15 14:13:55 +1000

    clean.bash removes precompiled libs
    closes #135

--- clean.bash
+++ clean.bash
@@ -1,3 +1,16 @@
 #!/bin/bash
 
-rm -rf extempore build/obj/{.deps,*}
+# rm -rf extempore build/obj/{.deps,*}
+
+# remove the precompiled libs from the stdlib
+
+for file in libs/*
+do
+   case "$file" in
+       libs/create-xtm.xtm ) continue;;
+       libs/xtm.xtm ) continue;;
+       libs/*.xtm ) rm -rf $file;;
+       libs/xtm*.dylib) rm -rf $file;;
+       libs/xtm*.so ) rm -rf $file;;
+   esac 
+done
