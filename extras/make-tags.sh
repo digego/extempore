3b51dd04ba2d667541591a328408477880cfeb3d
Ales Guzik <me@aguzik.net>
2014-10-17 13:43:41 +0300

    support building TAGS on Linux

--- extras/make-tags.sh
+++ extras/make-tags.sh
@@ -2,7 +2,12 @@
 
 [ -f TAGS ] && rm TAGS
 
+case $(uname) in
+  'Linux')  FIND_CMD='find . -regextype posix-extended';;
+  'Darwin') FIND_CMD='find -E .';;
+esac
+
 # make extempore/xtlang tags
-find -E . -regex '.*/.*\.(cpp|h)$' -print | etags -
-find -E . -regex '.*/.*\.xtm$' -print | etags --append --regex='/(bind-[a-z]* \([a-z-_!]+\)/\1/' --language=scheme -
-find -E . -regex '.*/.*\.xtm$' -print | etags --append --regex='/(bind-lib [a-z-_!]+ \([a-z-_!]+\)/\2/' -
+$FIND_CMD -regex '.*/.*\.(cpp|h)$' -print | etags -
+$FIND_CMD -regex '.*/.*\.xtm$' -print | etags --append --regex='/(bind-[a-z]* \([a-z-_!]+\)/\1/' --language=scheme -
+$FIND_CMD -regex '.*/.*\.xtm$' -print | etags --append --regex='/(bind-lib [a-z-_!]+ \([a-z-_!]+\)/\2/' -
