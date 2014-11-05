50318f959f6cd0382b4c5c45928af64b70cff56d
Ben Swift <benjamin.j.swift@gmail.com>
2014-11-05 09:43:13 -0800

    add "do you want to remove precompiled libs" prompt to clean.bash

--- clean.bash
+++ clean.bash
@@ -3,5 +3,11 @@
 # remove extempore build files
 rm -rf extempore build/obj/{.deps,*}
 
-# remove precompiled extempore libs (e.g. from the stdlib)
-# rm -rf extempore libs/*.{xtm,dylib,so}
+read -p "Do you want to remove the precompiled xtlang libraries as well? [y/N] " -n 1 -r
+echo
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+    # remove precompiled extempore libs (e.g. from the stdlib)
+    echo removing precompiled libs...
+    rm -rf extempore libs/*.{xtm,dylib,so}
+fi
