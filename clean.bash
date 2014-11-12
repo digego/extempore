caa6bc4fbe5f137aee83ad1f3a86b75fa5df9158
Ben Swift <benjamin.j.swift@gmail.com>
2014-11-12 00:39:45 -0800

    clean.bash update

--- clean.bash
+++ clean.bash
@@ -3,12 +3,12 @@
 # remove extempore build files
 rm -rf extempore build/obj/{.deps,*}
 
-if [[ $- == *i* ]]
-then
-    read -p "Do you want to remove the precompiled xtlang libraries as well? [y/N] " -n 1 -r
-else
-    REPLY=n # keep the precompiled libs if not in an interactive terminal
-fi    
+# if [[ $- == *i* ]]
+# then
+read -p "Do you want to remove the precompiled xtlang libraries as well? [y/N] " -n 1 -r
+# else
+#     REPLY=n # keep the precompiled libs if not in an interactive terminal
+# fi    
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
     # remove precompiled extempore libs (e.g. from the stdlib)
