152c260d88e304f46fa8461e6eb9e5f0df3553cd
Ben Swift <benjamin.j.swift@gmail.com>
2014-11-18 15:28:16 +1100

    bugfix exit -> std::_Exit
    on Linux, the exit code wasn't getting returned (it was always 1)

--- src/SchemeFFI.cpp
+++ src/SchemeFFI.cpp
@@ -612,10 +612,8 @@ namespace extemp {
 
     pointer SchemeFFI::exit_extempore(scheme* _sc, pointer args)
     {
-        // This is a seriously nasty HACK!!
-        // PLEASE FIX ME!
-      int64_t retval = ivalue(pair_car(args));
-      exit(retval);
+      int rc = (int)ivalue(pair_car(args));
+      std::_Exit(rc);
     }
 
     pointer SchemeFFI::dataGETi64(scheme* _sc, pointer args)
