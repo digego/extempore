86aeb8e85b7ec5378a3fe22e02805b2f8d58610e
Giorashh <giorashh@localhost>
2014-12-26 03:17:15 +0200

    make-tags.sh ignores hidden files

--- extras/make-tags.sh
+++ extras/make-tags.sh
@@ -8,7 +8,11 @@ else
   FIND_CMD='find -E .'
 fi
 
+# non-hidden files with extension xtm
+XTMFILE_REGEX='^.*/[^\.][^/]*\.xtm$'
+
 # make extempore/xtlang tags
-$FIND_CMD -regex '.*/.*\.(cpp|h)$' -print | etags -
-$FIND_CMD -regex '.*/.*\.xtm$' -print | etags --append --regex='/(bind-[a-z]* \([a-z-_!]+\)/\1/' --language=scheme -
-$FIND_CMD -regex '.*/.*\.xtm$' -print | etags --append --regex='/(bind-lib [a-z-_!]+ \([a-z-_!]+\)/\2/' -
+
+$FIND_CMD -regex '.*/[^\.].*\.(cpp|h)$' -print | etags -
+$FIND_CMD -regex "$XTMFILE_REGEX" -print | etags --append --regex='/(bind-[a-z]* \([a-z-_!]+\)/\1/' --language=scheme -
+$FIND_CMD -regex "$XTMFILE_REGEX" -print | etags --append --regex='/(bind-lib [a-z-_!]+ \([a-z-_!]+\)/\2/' -
