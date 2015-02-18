3dd0b8d5029266f9cde080785db2c063dc25a8d9
Ben Swift <ben@benswift.me>
2015-02-18 17:09:22 +1100

    update clean.bash to not require user interaction
    now pass a --precomp flag to clean the precompiled librares

--- clean.bash
+++ clean.bash
@@ -3,15 +3,7 @@
 # remove extempore build files
 rm -rf extempore build/obj/{.deps,*}
 
-# if [[ $- == *i* ]]
-# then
-read -p "Do you want to remove the precompiled xtlang libraries as well? [y/N] " -n 1 -r
-# else
-#     REPLY=n # keep the precompiled libs if not in an interactive terminal
-# fi    
-if [[ $REPLY =~ ^[Yy]$ ]]
-then
-    # remove precompiled extempore libs (e.g. from the stdlib)
-    echo removing precompiled libs...
-    rm -rf libs/*.{xtm,dylib,so}
+#!/bin/sh
+if [ "$1" == "--precomp" ]; then
+    rm -rf libs/*.{xtm,dylib,so} && echo removed precompiled libs.
 fi
