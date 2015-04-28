e4800b9aa49860a1174c681570ee6990a4d0e3a5
Ben Swift <ben@benswift.me>
2015-04-28 11:04:38 +1000

    update extempore-compile.sh to use LLVM 3.4.2

--- extras/extempore-compile.sh
+++ extras/extempore-compile.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 ROOTDIR="$(pwd)"
 # sudo apt-get install git g++ portaudio19-dev libpcre3-dev libgl1-mesa-dev
-wget -qO- http://llvm.org/releases/3.4.1/llvm-3.4.1.src.tar.gz | tar xvz && \
+wget -qO- http://llvm.org/releases/3.4.2/llvm-3.4.2.src.tar.gz | tar xvz && \
 git clone git@github.com:digego/extempore && \
-cd llvm-3.4.1.src/lib/AsmParser && \
+cd llvm-3.4.2.src/lib/AsmParser && \
 patch < $ROOTDIR/extempore/extras/llparser.patch && \
 mkdir $ROOTDIR/extempore/llvm-build && \
-cd $ROOTDIR/llvm-3.4.1.src && \
+cd $ROOTDIR/llvm-3.4.2.src && \
 ./configure --prefix=$ROOTDIR/extempore/llvm-build --enable-optimized --enable-curses=no --enable-terminfo=no && \
 make && make install && \
 cd $ROOTDIR/extempore && \
