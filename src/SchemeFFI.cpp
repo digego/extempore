10d521d3bb49dbb7f985c1421d7ae0b20d0b9634
Ben Swift <ben@benswift.me>
2015-05-09 12:08:24 +1000

    change dlopen mode back to RTLD_LAZY

--- src/SchemeFFI.cpp
+++ src/SchemeFFI.cpp
@@ -1161,7 +1161,7 @@ namespace extemp {
 	  mbstowcs_s(&converted_chars,wstr,ccc_size, ccc, _TRUNCATE);
       void* lib_handle = LoadLibrary(wstr);
 #else
-      void* lib_handle = dlopen(string_value(pair_car(args)), RTLD_NOW|RTLD_GLOBAL);
+      void* lib_handle = dlopen(string_value(pair_car(args)), RTLD_LAZY|RTLD_GLOBAL);
 #endif
 	if (!lib_handle)
 	{
