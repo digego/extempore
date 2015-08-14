9f8cde8d436dcdf6170e5d32bc2382c5994efadb
Ben Swift <ben@benswift.me>
2015-08-14 12:38:28 +1000

    add "alternate port" support to integration-test.sh

--- integration-test.sh
+++ integration-test.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
 TEST_DIR=$PWD/integration-test
+# port to run the Extempore primary process on
+TEST_PORT=17097
 
 if [ ! -d $TEST_DIR ]; then
     mkdir $TEST_DIR
@@ -12,7 +14,7 @@ echo "Running tests in ${TEST_DIR}..."
 
 # without MCJIT
 
-cmake -DCMAKE_INSTALL_PREFIX=$TEST_DIR -DMCJIT=OFF .. && make clean && make && make install && ./bin/extempore --sharedir $TEST_DIR/share/extempore --run tests/all.xtm
+cmake -DCMAKE_INSTALL_PREFIX=$TEST_DIR -DMCJIT=OFF .. && make clean && make && make install && ./bin/extempore --port=${TEST_PORT} --sharedir $TEST_DIR/share/extempore --run tests/all.xtm
 
 if (($? != 0)); then
     echo -e "\033[0;31mIntegration test failed (AOT:false, MCJIT:false) $f\033[0;00m"
@@ -22,7 +24,7 @@ fi
 
 # aot-compile the stdlib, then run all the tests again
 
-AOT_COMPILATION_COMMAND="${TEST_DIR}/bin/extempore --nostd $1 --eval " ../compile-stdlib.sh && ./bin/extempore --sharedir $TEST_DIR/share/extempore --run tests/all.xtm
+AOT_COMPILATION_COMMAND="${TEST_DIR}/bin/extempore --port=${TEST_PORT} --nostd $1 --eval " ../compile-stdlib.sh && ./bin/extempore --port=${TEST_PORT} --sharedir $TEST_DIR/share/extempore --run tests/all.xtm
 
 if (($? != 0)); then
     echo -e "\033[0;31mIntegration test failed (AOT:true, MCJIT:false) $f\033[0;00m"
@@ -32,7 +34,7 @@ fi
 
 # repeat the above steps, this time with MCJIT
 
-cmake -DCMAKE_INSTALL_PREFIX=$TEST_DIR -DMCJIT=ON .. && make clean && make && make install && ./bin/extempore --sharedir $TEST_DIR/share/extempore --run tests/all.xtm
+cmake -DCMAKE_INSTALL_PREFIX=$TEST_DIR -DMCJIT=ON .. && make clean && make && make install && ./bin/extempore --port=${TEST_PORT} --sharedir $TEST_DIR/share/extempore --run tests/all.xtm
 
 if (($? != 0)); then
     echo -e "\033[0;31mIntegration test failed (AOT:false, MCJIT:true) $f\033[0;00m"
@@ -40,7 +42,7 @@ if (($? != 0)); then
     exit 1
 fi
 
-AOT_COMPILATION_COMMAND="${TEST_DIR}/bin/extempore --nostd $1 --eval " ../compile-stdlib.sh && ./bin/extempore --sharedir $TEST_DIR/share/extempore --run tests/all.xtm
+AOT_COMPILATION_COMMAND="${TEST_DIR}/bin/extempore --port=${TEST_PORT} --nostd $1 --eval " ../compile-stdlib.sh && ./bin/extempore --port=${TEST_PORT} --sharedir $TEST_DIR/share/extempore --run tests/all.xtm
 
 if (($? != 0)); then
     echo -e "\033[0;31mIntegration test failed (AOT:true, MCJIT:true) $f\033[0;00m"
