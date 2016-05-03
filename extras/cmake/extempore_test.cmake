bdb54db22fa4e35ae547e60b075f073e5b514325
Ben Swift <ben@benswift.me>
2016-05-03 11:58:49 +1000

    more extempore_test.cmake updates
    man, either ctest is poorly documented or I'm an idiot.

--- extras/cmake/extempore_test.cmake
+++ extras/cmake/extempore_test.cmake
@@ -13,14 +13,11 @@ elseif(WIN32)
   set(CTEST_BASE_DIRECTORY "$ENV{TEMP}/extempore-ctest")
 endif()
 
-set(CTEST_SOURCE_DIRECTORY "${CTEST_BASE_DIRECTORY}/source")
-set(CTEST_BINARY_DIRECTORY "${CTEST_BASE_DIRECTORY}/build")
-
-file(MAKE_DIRECTORY ${CTEST_SOURCE_DIRECTORY})
-file(MAKE_DIRECTORY ${CTEST_BINARY_DIRECTORY})
+file(MAKE_DIRECTORY ${CTEST_BASE_DIRECTORY}/source)
+file(MAKE_DIRECTORY ${CTEST_BASE_DIRECTORY}/build)
 
 set(CTEST_BUILD_NAME "${CMAKE_SYSTEM_NAME}-${CMAKE_SYSTEM_VERSION}-${CMAKE_SYSTEM_PROCESSOR}")
-set(CTEST_BUILD_CONFIGURATION "Release")
+# set(CTEST_BUILD_CONFIGURATION "Release")
 
 if(UNIX)
   set(CTEST_CMAKE_GENERATOR "Unix Makefiles")
@@ -28,22 +25,24 @@ elseif(WIN32)
   set(CTEST_CMAKE_GENERATOR "Visual Studio 14 2015 Win64")
 endif()
 
-if(NOT EXISTS "${CTEST_SOURCE_DIRECTORY}")
-  set(CTEST_CHECKOUT_COMMAND "${CTEST_GIT_COMMAND} clone https://github.com/digego/extempore.git ${CTEST_SOURCE_DIRECTORY}")
+if(NOT EXISTS "${CTEST_BASE_DIRECTORY}/source")
+  set(CTEST_CHECKOUT_COMMAND "${CTEST_GIT_COMMAND} clone https://github.com/digego/extempore.git ${CTEST_BASE_DIRECTORY}/source")
 endif()
 
-set(CTEST_CONFIGURE_COMMAND "${CMAKE_COMMAND} \"-G${CTEST_CMAKE_GENERATOR}\" -DCMAKE_BUILD_TYPE=${CTEST_BUILD_CONFIGURATION} -DBUILD_TESTS=ON \"${CTEST_SOURCE_DIRECTORY}\"")
-# set(CTEST_UPDATE_COMMAND "${CTEST_GIT_COMMAND} pull")
+ctest_start(Continuous "${CTEST_BASE_DIRECTORY}/source" "${CTEST_BASE_DIRECTORY}/build")
 
-ctest_start(Continuous)
-ctest_update(RETURN_VALUE count)
+ctest_update(SOURCE ${CTEST_BASE_DIRECTORY}/source RETURN_VALUE)
 
-if(count GREATER 0)
-  # if master has been updated, run test and upload results
-  ctest_configure()
-  ctest_build()
-  ctest_test()
-  ctest_submit()
-else()
-  message(STATUS "No changes detected, current test results are up-do-date")
-endif()
+ctest_configure(
+  BUILD ${CTEST_BASE_DIRECTORY}/build
+  SOURCE ${CTEST_BASE_DIRECTORY}/source
+  OPTIONS "-DBUILD_TESTS=ON")
+
+ctest_build(
+  BUILD ${CTEST_BASE_DIRECTORY}/build
+  FLAGS -j4
+  CONFIGURATION Release)
+
+ctest_test(BUILD ${CTEST_BASE_DIRECTORY}/build)
+
+ctest_submit()
