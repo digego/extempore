8df6a769af84143ceb31d297b0dd3f56ccd9a50d
Ben Swift <ben@benswift.me>
2016-05-04 11:18:58 +1000

    bugfixes for ctest on Windows

--- extras/cmake/extempore_test.cmake
+++ extras/cmake/extempore_test.cmake
@@ -14,11 +14,11 @@ elseif(WIN32)
   set(CTEST_BASE_DIRECTORY "$ENV{TEMP}/extempore-ctest")
 endif()
 
-# file(MAKE_DIRECTORY ${CTEST_BASE_DIRECTORY}/source)
-# file(MAKE_DIRECTORY ${CTEST_BASE_DIRECTORY}/build)
+if(NOT EXISTS "${CTEST_BASE_DIRECTORY}/source")
+  set(CTEST_CHECKOUT_COMMAND "${CTEST_GIT_COMMAND} clone https://github.com/digego/extempore.git source")
+endif()
 
 set(CTEST_BUILD_NAME "${CMAKE_SYSTEM_NAME}-${CMAKE_SYSTEM_VERSION}-${CMAKE_SYSTEM_PROCESSOR}")
-# set(CTEST_BUILD_CONFIGURATION "Release")
 
 if(UNIX)
   set(CTEST_CMAKE_GENERATOR "Unix Makefiles")
@@ -26,24 +26,19 @@ elseif(WIN32)
   set(CTEST_CMAKE_GENERATOR "Visual Studio 14 2015 Win64")
 endif()
 
-if(NOT EXISTS "${CTEST_BASE_DIRECTORY}/source")
-  set(CTEST_CHECKOUT_COMMAND "${CTEST_GIT_COMMAND} clone https://github.com/digego/extempore.git ${CTEST_BASE_DIRECTORY}/source")
-endif()
-
 ctest_start(Continuous "${CTEST_BASE_DIRECTORY}/source" "${CTEST_BASE_DIRECTORY}/build")
 
-ctest_update(SOURCE ${CTEST_BASE_DIRECTORY}/source)
+ctest_update(SOURCE "${CTEST_BASE_DIRECTORY}/source")
 
 ctest_configure(
-  BUILD ${CTEST_BASE_DIRECTORY}/build
-  SOURCE ${CTEST_BASE_DIRECTORY}/source
+  BUILD "${CTEST_BASE_DIRECTORY}/build"
+  SOURCE "${CTEST_BASE_DIRECTORY}/source"
   OPTIONS "-DBUILD_TESTS=ON")
 
 ctest_build(
-  BUILD ${CTEST_BASE_DIRECTORY}/build
-  FLAGS -j4
+  BUILD "${CTEST_BASE_DIRECTORY}/build"
   CONFIGURATION Release)
 
-ctest_test(BUILD ${CTEST_BASE_DIRECTORY}/build)
+ctest_test(BUILD "${CTEST_BASE_DIRECTORY}/build")
 
 ctest_submit()
