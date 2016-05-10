5680bbf47332274ae5824e7b4a563470a84e3b17
Ben Swift <ben@benswift.me>
2016-05-10 14:49:32 +1000

    bugfix for sitename detection for CTest

--- extras/cmake/extempore_test.cmake
+++ extras/cmake/extempore_test.cmake
@@ -1,4 +1,5 @@
 set(CTEST_PROJECT_NAME Extempore)
+site_name(CTEST_SITE)
 set(CTEST_DROP_METHOD "http")
 set(CTEST_DROP_SITE "my.cdash.org")
 set(CTEST_DROP_LOCATION "/submit.php?project=Extempore")
