d868ed97e4a59c719d2da04964209f40882728bb
Ben Swift <ben@benswift.me>
2016-05-10 10:34:13 +1000

    set cdash site name to machine name

--- extras/cmake/extempore_test.cmake
+++ extras/cmake/extempore_test.cmake
@@ -1,5 +1,4 @@
 set(CTEST_PROJECT_NAME Extempore)
-set(CTEST_SITE "Extempore")
 set(CTEST_DROP_METHOD "http")
 set(CTEST_DROP_SITE "my.cdash.org")
 set(CTEST_DROP_LOCATION "/submit.php?project=Extempore")
