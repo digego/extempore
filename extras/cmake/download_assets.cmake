c8d96ad8e8afbc334c710a683a34d0259cab20a6
Ben Swift <ben@benswift.me>
2020-03-24 18:20:47 +1100

    more tweaks to the asset downloading stuff
    an attempt to get it to work with both package and non-package targets
    
    sharedir shenanigans abound with this stuff

--- extras/cmake/download_assets.cmake
+++ extras/cmake/download_assets.cmake
@@ -1,6 +1,15 @@
 set(ASSETS_DOWNLOAD_PATH ${CMAKE_BINARY_DIR}/assets.tar.gz)
 set(ASSETS_GIT_REF 0c9f32c)
 
+if(EXISTS ${CMAKE_SOURCE_DIR}/assets)
+  message(FATAL_ERROR "Directory ${CMAKE_SOURCE_DIR}/assets already exists. You must delete or move it so that the build process can download the latest assets folder from GitHub.")
+endif()
+
+# deleting stuff in the build directory is fair game from here (since we're already protected against the problem of in-tree builds with the previous fatal error)
+if(EXISTS ${CMAKE_BINARY_DIR}/assets)
+  file(REMOVE_RECURSE ${CMAKE_BINARY_DIR}/assets)
+endif()
+
 message(STATUS "https://github.com/extemporelang/extempore-assets" )
 file(DOWNLOAD
   https://api.github.com/repos/extemporelang/extempore-assets/tarball/${ASSETS_GIT_REF}
@@ -12,14 +21,9 @@ file(DOWNLOAD
 # untar it with CMake's built-in untar command
 execute_process(COMMAND ${CMAKE_COMMAND} -E tar xz ${ASSETS_DOWNLOAD_PATH})
 
-# if there's a previously downloaded assets file in in your build directory (or
-# wherever you're running cmake) then remove it
-# NOTE: this is bad for in-tree builds. probably should add a warning or something.
-if(EXISTS ${CMAKE_BINARY_DIR}/assets)
-  file(REMOVE_RECURSE ${CMAKE_BINARY_DIR}/assets)
-endif()
-
-# rename the folder to just "assets"
+# rename folder to just "assets", move into source directory
 file(RENAME
   ${CMAKE_BINARY_DIR}/extemporelang-extempore-assets-${ASSETS_GIT_REF}
-  ${CMAKE_BINARY_DIR}/assets)
+  ${CMAKE_SOURCE_DIR}/assets)
+
+message(STATUS "moving assets to ${CMAKE_SOURCE_DIR}/assets")
