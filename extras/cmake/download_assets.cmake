811e993e7f3c7ee66b5280d09b1cb2bb9a883822
Ben Swift <ben@benswift.me>
2020-03-25 13:46:28 +1100

    add CMAKE_BINARY_DIR

--- extras/cmake/download_assets.cmake
+++ extras/cmake/download_assets.cmake
@@ -1,6 +1,3 @@
-# set(ASSETS_PATH ${CMAKE_BINARY_DIR}/assets.tar.gz)
-# set(ASSETS_GIT_REF 0c9f32c)
-
 message(STATUS "Downloading assets from https://github.com/extemporelang/extempore-assets")
 
 if(EXISTS ${ASSETS_PATH})
@@ -9,13 +6,13 @@ endif()
 
 file(DOWNLOAD
   https://api.github.com/repos/extemporelang/extempore-assets/tarball/${ASSETS_GIT_REF}
-  assets.tar.gz
+  ${CMAKE_BINARY_DIR}/assets.tar.gz
   # options
   INACTIVITY_TIMEOUT 60
   SHOW_PROGRESS)
 
 # untar it with CMake's built-in untar command
-execute_process(COMMAND ${CMAKE_COMMAND} -E tar xz assets.tar.gz)
+execute_process(COMMAND ${CMAKE_COMMAND} -E tar xz ${CMAKE_BINARY_DIR}/assets.tar.gz)
 
 # rename folder to just "assets", move into source directory
 file(RENAME
